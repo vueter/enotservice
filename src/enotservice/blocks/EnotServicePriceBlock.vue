9549f83327347193897cdb4bcb89fbeba6ee2331
Beknazarov <tensor2flow@gmail.com>
2019-05-31 16:06:53 +0500

    price

--- src/enotservice/blocks/EnotServicePriceBlock.vue
+++ src/enotservice/blocks/EnotServicePriceBlock.vue
@@ -5,26 +5,38 @@
 			<center>
 				<p class="block-sub-title">Стоимость уборки квартиры напрямую зависит от нескольких критериев, таких как площадь помещения, колиество комнат и санузлов. Так же необходимо принять в учет тип планируемой уборки.</p>
 			</center>
-			<v-layout row wrap>
-				<v-flex md3 xs12 sm12 pa-1>
-					<es-price-card image="room-1" text="1 комнатная" price="от 40000 сум"/>
+			<v-layout row wrap v-if="schema !== null">
+				<v-flex md3 xs12 sm12 pa-1 v-for="(item, index) in schema[0].body[1].items" v-bind:key="index">
+					<es-price-card v-bind:image="'room-' + (index + 1)" v-bind:text="item.text" v-bind:price="'от ' + item.price + ' сум'"/>
 				</v-flex>
 				<v-flex md3 xs12 sm12 pa-1>
-					<es-price-card image="room-2" text="2-х комнатная" price="от 60000 сум"/>
-				</v-flex>
-				<v-flex md3 xs12 sm12 pa-1>
-					<es-price-card image="room-3" text="3-х комнатная" price="от 80000 сум"/>
-				</v-flex>
-				<v-flex md3 xs12 sm12 pa-1>
-					<es-price-card image="home" text="Свой дом" price="от 90000 сум"/>
+					<es-price-card image="home" text="Свой дом" v-bind:price="'от ' + schema[0].body[0].items[1].price + ' сум'"/>
 				</v-flex>
 			</v-layout>
 		</v-container>
 	</div>
 </template>
 <script>
+import axios from 'axios'
 export default {
-	name: 'es-price-block'
+	name: 'es-price-block',
+	data: () => ({
+		schema: null
+	}),
+	mounted(){
+		axios({
+			method: 'GET',
+			url: 'http://localhost:3000/schema',
+			headers: {
+				'Content-Type': 'application/json',
+				'Accept-Version': '1.x'
+			}
+		}).then(response => {
+			if(response.data.error === 'Ok'){
+				this.schema = response.data.data
+			}
+		})
+	}
 }
 </script>
 
